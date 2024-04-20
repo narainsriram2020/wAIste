69bcf487f55ff430c13ae8b3c95ea0a37f178d65
Arav Luthra <aravluthra@Aravs-MacBook-Air.local>
2024-04-20 00:43:27 -0400

    changes added

---
+++ waiste/lib/Tflitemodel.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+void main() {
+  runApp(const TfliteModel());
+}
+
+class TfliteModel extends StatefulWidget {
+  const TfliteModel({super.key});
+
+  @override
+  State<TfliteModel> createState() => _TfliteModelState();
+}
+
+class _TfliteModelState extends State<TfliteModel> {
+
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  Future loadModel() {
+    Tflite.close();
+    Tflite.close();
+    String res;
+    res = await Tflite.loadModel(model: model)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
